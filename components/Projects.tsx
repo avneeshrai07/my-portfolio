acea92d1423f7a1e59e770f96df7c787631c88dc
avneeshrai07 <ofc.avneesh@gmail.com>
2026-04-05 01:11:45 +0530

    build done

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,7 +4,7 @@ import { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import projectsData from "@/data/projects.json";
-import { Safari } from "@/components/ui/safari.tsx";
+import { Safari } from "@/components/ui/safari";
 
 gsap.registerPlugin(ScrollTrigger);
 
