29dc4695757f68a9525d76e57443619b1d7b899a
avneeshrai07 <ofc.avneesh@gmail.com>
2026-04-12 23:25:03 +0530

    added projects

--- app/page.tsx
+++ app/page.tsx
@@ -3,32 +3,23 @@ import MusicSection from "@/components/MusicSection";
 import MovieSection from "@/components/MoviesSection";
 import Skills from '@/components/Skills'
 import MyJourney from "@/components/MyJourney";
-import ProjectsCarousel from '@/components/projects/ProjectsCarousel'
-// import BookCard from '@/components/ProjectCard'
 import ProjectsSection from '@/components/ProjectsSection';
 export default function Home() {
 
   return (
     <main className="min-h-screen  ">
-      {/* Hero Section */}
+
       <HeroSection />
 
-      
-      
-      {/* Music Section */}
       <Skills/>
 
       <ProjectsSection/>
-      
+
       <MusicSection />
 
       <MovieSection />
       
       <MyJourney/>
-
-      
-      <ProjectsCarousel />
- 
 
     </main>
   );
