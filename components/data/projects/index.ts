14c66efec0d55b71f6e8fd75069840da8109be4c
avneeshrai07 <ofc.avneesh@gmail.com>
2026-04-12 08:14:16 +0530

    removed projects

--- components/data/projects/index.ts
+++
@@ -1,9 +0,0 @@
-import { sunakku } from "./sunakku";
-import { myproject2 } from "./myproject2"; 
-
-import type { ProjectConfig } from "@/types/project";
-
-export const allProjects: ProjectConfig[] = [
-  sunakku,
-  myproject2,
-];
